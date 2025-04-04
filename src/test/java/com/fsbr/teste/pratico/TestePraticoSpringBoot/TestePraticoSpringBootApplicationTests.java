727e70caa5421ec07e2485bfaf4261732f7abf76
Luiz Neto <luizcsneto@outlook.com>
2025-04-04 13:19:44 -0300

    feat: configuração inicial do projeto

---
+++ src/test/java/com/fsbr/teste/pratico/TestePraticoSpringBoot/TestePraticoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsbr.teste.pratico.TestePraticoSpringBoot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
